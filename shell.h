aea62387448d5b6a3d258ffac3cbafef08b4dd1d
Setty23 <125440661+Setty23@users.noreply.github.com>
2023-08-24 00:34:55 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -198,7 +198,7 @@ int renumber_history(info_t *info);
 /* _lists1.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
+size_t printlist_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
