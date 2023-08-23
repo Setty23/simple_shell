ee8e99a624f4f5436bf1c73b96e559a76fa29d4a
Setty23 <125440661+Setty23@users.noreply.github.com>
2023-08-24 00:53:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -44,3 +44,20 @@ int main(int ac, char **av)
 	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
+
+/**
+ * bfree - function that frees thepointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, if not return 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
